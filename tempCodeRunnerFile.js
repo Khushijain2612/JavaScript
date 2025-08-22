4c49dc7427fe4ba5480e2f947676e8375bc7b94d
Khushi jain <Khushijain.kj2612@gmail.com>
2025-08-22 16:18:39 +0530

    Problem 2

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,11 +1,3 @@
-let marks = [25, 42, 18, 55, 38, 60];
-let  doubled= marks.map(mark=>mark*2);
-console.log(doubled);
-let pass= marks.filter(num=>num>=40);
-console.log(pass);
-let total= marks.reduce((total,num)=>total+num,0);
-console.log(total);
-// Problem 2
 let students = [
   { name: "Khushi", marks: 78 },
   { name: "Riya", marks: 92 },
